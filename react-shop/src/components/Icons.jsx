3097dab37ce1d00e4e1bc9813b2058702e638cce
makas007 <maximus.makas@gmail.com>
2021-10-13 17:16:47 +0300

    icons

---
+++ react-shop/src/components/Icons.jsx
@@ -0,0 +1,6 @@
+export const Flag = () => {<svg width="28" height="20" viewBox="0 0 28 20" fill="black" xmlns="http://www.w3.org/2000/svg">
+<svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="28" height="20" fill="#157CBB"/>
+</svg>
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M0 12H8V20H12V12H28V8H12V0H8V8H0V12Z" fill="#FFD34D"/>
+</svg>}
