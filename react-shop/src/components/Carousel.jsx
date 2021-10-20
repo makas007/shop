0240115f993cf4d53b7ea8d4f6ab14b0e7874696
maxOn <maximus.makas@gmail.com>
2021-10-20 17:21:02 +0300

    carousel fixed

--- react-shop/src/components/Carousel.jsx
+++ react-shop/src/components/Carousel.jsx
@@ -2,40 +2,36 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import  {ReactComponent as SliderItem1}  from "../icons/slider-item3.svg";
+
 
 export default class MultipleRows extends Component {
   render() {
     const settings = {
       dots: true,
       infinite: true,
-      speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      pauseOnHover: true
+
     };
     return (
-      <div>
-        <h2> Single Item</h2>
+      <div className="container">
         <Slider {...settings}>
           <div>
-            <SliderItem1/>
+            <img src="/images/image_1.png" alt=""/>
           </div>
           <div>
-            <SliderItem1/>
+            <img src="/images/image_1.png" alt=""/>
           </div>
           <div>
-            <SliderItem1/>
-          </div>
-          <div>
-            <SliderItem1/>
-          </div>
-          <div>
-            <SliderItem1/>
-          </div>
-          <div>
-            <SliderItem1/>
+            <img src="/images/image_1.png" alt=""/>
           </div>
         </Slider>
+        <p className="slider-text">Unique & exlusive products made by independant.
+          Designers craftmen, artist & creators who care about the environment
+        </p>
       </div>
     );
   }
